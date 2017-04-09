6b2d9a70099f9c0f01512f5343c563a7a39b8d27
Marc Piechura <piechura@gmx.net>
2017-04-09 21:06:23 +0200

    Example of joining a list of id's with a second source

---
+++ DynamicData.Snippets/Join/JoinBasedOnListOfIds.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace DynamicData.Snippets.Join
+{
+    class JoinBasedOnListOfIds
+    {
+        public JoinBasedOnListOfIds(IObservableCache<User, int> users, IObservableCache<Role, int> roles)
+        {
+            //users.Connect().JoinMany(roles.Connect(),
+            //        // select some sort of list with ids 
+            //        user => user.Roles,
+            //        // select right key
+            //        role => role.Id,
+            //        // join every list of every user with the right source and produce a list of matching values
+            //        (user, roles) => new UserViewModel
+            //        {
+            //            Name = user.Name,
+            //            Roles = roles
+            //        });
+
+        }
+    }
+    
+    class User
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public List<int> Roles { get; set; }
+    }
+
+    class Role
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+
+    class UserViewModel
+    {
+        public string Name { get; set; }
+
+        public List<Role> Roles { get; set; }
+    }
+}
