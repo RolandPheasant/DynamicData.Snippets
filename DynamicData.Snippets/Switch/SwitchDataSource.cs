526797cca02efb2c34fe84c16f8786fb4a650ccf
Roland Pheasant <roland@dynamic-data.org>
2017-04-20 20:53:33 +0100

    Switch data source example

---
+++ DynamicData.Snippets/Switch/SwitchDataSource.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Reactive.Linq;
+using DynamicData.Binding;
+
+namespace DynamicData.Snippets.Switch
+{
+    public enum SwitchDataSourceOption
+    {
+        SourceA,
+        SourceB
+    }
+
+    public sealed class SwitchDataSource: AbstractNotifyPropertyChanged, IDisposable
+    {
+        private SwitchDataSourceOption _option;
+        private readonly IDisposable _cleanUp;
+
+        public IObservableList<int> DataSource { get; }
+
+        public SwitchDataSource(IObservableList<int> sourceA, IObservableList<int> sourceB)
+        {
+            /*
+             * Switching data acts on IObservable<IChangSet<T>> or IObservable<IObservableList<T>>
+             * 
+             * The same concept applies to the ObservableCache
+             */
+
+            DataSource = this.WhenValueChanged(@this => @this.Option)
+                .Select(opt => opt == SwitchDataSourceOption.SourceA ? sourceA : sourceB)
+                .Switch()
+                .AsObservableList();
+
+            _cleanUp = DataSource;
+        }
+        
+        public SwitchDataSourceOption Option
+        {
+            get => _option;
+            set => SetAndRaise(ref _option, value);
+        }
+
+        public void Dispose()
+        {
+            _cleanUp.Dispose();
+        }
+    }
+}
---
+++ DynamicData.Snippets/Switch/SwitchDataSourceFixture.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit;
+using NUnit.Framework;
+
+namespace DynamicData.Snippets.Switch
+{
+    [TestFixture]
+    public class SwitchDataSourceFixture
+    {
+        [Test]
+        public void Switch()
+        {
+            using (var listA = new SourceList<int>())
+            using (var listB = new SourceList<int>())
+            using (var sut = new SwitchDataSource(listA, listB))
+            {
+                var oddNumbers = Enumerable.Range(1, 1000).Where(i => i % 2 == 1).ToArray();
+                var evenNumbers = Enumerable.Range(1, 1000).Where(i => i % 2 == 2).ToArray();
+
+                listA.AddRange(oddNumbers);
+                listB.AddRange(evenNumbers);
+
+                sut.DataSource.Items.ShouldAllBeEquivalentTo(oddNumbers);
+
+                sut.Option = SwitchDataSourceOption.SourceB;
+                sut.DataSource.Items.ShouldAllBeEquivalentTo(evenNumbers);
+
+                sut.Option = SwitchDataSourceOption.SourceA;
+                sut.DataSource.Items.ShouldAllBeEquivalentTo(oddNumbers);
+            }
+        }
+    }
+}
