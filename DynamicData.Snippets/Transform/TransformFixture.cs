8503dc243e6391d563596b5594242fe4e21c39d6
Damien Chaib <dchaib@users.noreply.github.com>
2019-03-01 22:24:39 +0100

    Added (failing) fact for TransformMany with projection

--- DynamicData.Snippets/Transform/TransformFixture.cs
+++ DynamicData.Snippets/Transform/TransformFixture.cs
@@ -53,5 +53,67 @@ namespace DynamicData.Snippets.Transform
                 sut.Children.Items.ShouldBeEquivalentTo(parents.SelectMany(p => p.Children));
             }
         }
+
+        [Fact]
+        public void FlattenObservableCollectionWithProjection()
+        {
+            var children = new[]
+            {
+                new NestedChild("A", "ValueA"),
+                new NestedChild("B", "ValueB"),
+                new NestedChild("C", "ValueC"),
+                new NestedChild("D", "ValueD"),
+                new NestedChild("E", "ValueE"),
+                new NestedChild("F", "ValueF")
+            };
+
+            var parents = new[]
+            {
+                new ClassWithNestedObservableCollection(1, new[] { children[0], children[1] }),
+                new ClassWithNestedObservableCollection(2, new[] { children[2], children[3] }),
+                new ClassWithNestedObservableCollection(3, new[] { children[4] })
+            };
+
+            using (var source = new SourceCache<ClassWithNestedObservableCollection, int>(x => x.Id))
+            using (var sut = source.Connect()
+                        .TransformMany(parent => parent.Children.Select(c => new ProjectedNestedChild(parent, c)), c => c.Child.Name)
+                        .AsObservableCache())
+            {
+                source.AddOrUpdate(parents);
+
+                sut.Count.Should().Be(5);
+                sut.Items.ShouldBeEquivalentTo(parents.SelectMany(p => p.Children.Take(5)));
+
+                //add a child to the observable collection
+                parents[2].Children.Add(children[5]);
+
+                sut.Count.Should().Be(6);
+                sut.Items.ShouldBeEquivalentTo(parents.SelectMany(p => p.Children));
+
+                //remove a parent and check children have moved
+                source.RemoveKey(1);
+                sut.Count.Should().Be(4);
+                sut.Items.ShouldBeEquivalentTo(parents.Skip(1).SelectMany(p => p.Children));
+
+                //add a parent and check items have been added back in
+                source.AddOrUpdate(parents[0]);
+
+                sut.Count.Should().Be(6);
+                sut.Items.ShouldBeEquivalentTo(parents.SelectMany(p => p.Children));
+            }
+        }
+
+        private class ProjectedNestedChild
+        {
+            public ClassWithNestedObservableCollection Parent { get; }
+
+            public NestedChild Child { get; }
+
+            public ProjectedNestedChild(ClassWithNestedObservableCollection parent, NestedChild child)
+            {
+                Parent = parent;
+                Child = child;
+            }
+        }
     }
 }
